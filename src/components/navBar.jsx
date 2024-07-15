406d503c4024bb24c97ab08ee00e0425c69a2e3d
Allen Jiang <allenjiang17@gmail.com>
2024-07-14 22:53:09 -0400

    few fixes

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,11 +3,11 @@ import { GameContext } from "../game";
 
 
 
-export default function NavBar() {
+export default function NavBar({setPage}) {
     const {ntask, day} = useContext(GameContext);
 
     return (
-        <div className="flex flex-row justify-between">
+        <div className="w-full flex flex-row justify-between">
             <div className="flex flex-row gap-5 justify-start">
                 <button className="hover:underline" onClick={()=>{setPage("map")}}>Main Map</button>
                 <button className="hover:underline" onClick={()=>{setPage("character")}}>Character</button>
