b2cc42de0bac9558f1520c35e3dab55ff0a1bce6
Allen Jiang <allenjiang17@gmail.com>
2024-07-20 13:13:48 -0400

    updated sound url

--- src/components/mainBody.jsx
+++ src/components/mainBody.jsx
@@ -14,7 +14,7 @@ export default function MainBody({page}) {
             <div>
                 <Map/>
                 <Sound 
-                    url="/sounds/Nothing_but_the_blood.mp3"
+                    url="/theSearchForEden/sounds/Nothing_but_the_blood.mp3"
                     playStatus={Sound.status.PLAYING}
                     volume={30}
                     loop={true} />
